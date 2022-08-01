adae60682b49404b9ecb5bfa47e4e47b9feadfbb
wagnerlmichael <wagnerleemichael@gmail.com>
2022-08-01 14:47:08 -0400

    added __main__, added input and output to docstring, and changed directory variables to function inputs

--- dbdiagram_syntax/dbdiagram_syntax.py
+++ dbdiagram_syntax/dbdiagram_syntax.py
@@ -1,8 +1,7 @@
 import os
 
 
-if __name__ == '__main__':
-
+def txt_to_dbdiagram_syntax(txts_location, db_diagram_location):
     """
     This is a complementary program to the psql_ops/create_diagram/create_diagram.py file. That file creates
     table syntax for sql in the form of txt files. This script will edit those txt files
@@ -11,11 +10,9 @@ if __name__ == '__main__':
     This script will collect all txt files in the directory input, and
     edit them to approximate the syntax for tables in db diagram.
 
+    Input: Directory where all original sql syntax txts will be collected
+    Output: Directory where txts edited for db.diagram will be stored
     """
-
-    txts_location = input("Input the path where txt files are stored: ")
-
-    db_diagram_location = input("Input the path where the new dbdiagram txt files will be stored: ")
 
     os.chdir(txts_location)
 
@@ -37,3 +34,12 @@ if __name__ == '__main__':
 
         with open(db_diagram_location + '/dbdiagram_' + i, 'w') as f:
             f.write(txt_new)
+
+
+if __name__ == '__main__':
+
+    input_dir = input("Input the path where txt files are stored: ")
+
+    output_dir = input("Input the path where the new dbdiagram txt files will be stored: ")
+
+    txt_to_dbdiagram_syntax(txts_location=input_dir, db_diagram_location=output_dir)
